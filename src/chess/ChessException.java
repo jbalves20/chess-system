ac183243f9afcc0a434d7bf49c2f3f8dc05581f0
João <joao.jr.alves20@gmail.com>
2019-07-06 14:29:50 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chess;
+
+/**
+ *
+ * @author João Alves
+ */
+public class ChessException extends RuntimeException {
+
+    public ChessException(String message) {
+        super(message);
+    }
+}
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -37,32 +37,72 @@ public class ChessMatch {
         return mat;
     }
     
+    private void placeNewPiece(char column, int row, ChessPiece piece){
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+    
     private void initialSetup(){
-        for(int i=0; i<board.getColumns(); i++){
-            if(i == 4){
-                setupPiece('q', i);
+        Color black = Color.BLACK;
+        Color white = Color.WHITE;
+        
+        for(int i = 0; i < board.getColumns(); i++){
+            switch(i){
+                    case 0:
+                        placeNewPiece('a', 8, new Rook(board, black));
+                        placeNewPiece('a', 7, new Pawn(board, black));
+                        placeNewPiece('a', 2, new Pawn(board, white));
+                        placeNewPiece('a', 1, new Rook(board, white));
+                        break;
+                    case 1:
+                        placeNewPiece('b', 8, new Knight(board, black));
+                        placeNewPiece('b', 7, new Pawn(board, black));
+                        placeNewPiece('b', 2, new Pawn(board, white));
+                        placeNewPiece('b', 1, new Knight(board, white));
+                        break;
+                    case 2:
+                        placeNewPiece('c', 8, new Bishop(board, black));
+                        placeNewPiece('c', 7, new Pawn(board, black));
+                        placeNewPiece('c', 2, new Pawn(board, white));
+                        placeNewPiece('c', 1, new Bishop(board, white));
+                        break;
+                    case 3:
+                        placeNewPiece('d', 8, new King(board, black));
+                        placeNewPiece('d', 7, new Pawn(board, black));
+                        placeNewPiece('d', 2, new Pawn(board, white));
+                        placeNewPiece('d', 1, new King(board, white));
+                        break;
+                    case 4:
+                        placeNewPiece('e', 8, new Queen(board, black));
+                        placeNewPiece('e', 7, new Pawn(board, black));
+                        placeNewPiece('e', 2, new Pawn(board, white));
+                        placeNewPiece('e', 1, new Queen(board, white));
+                        break;
+                    case 5:
+                        placeNewPiece('f', 8, new Bishop(board, black));
+                        placeNewPiece('f', 7, new Pawn(board, black));
+                        placeNewPiece('f', 2, new Pawn(board, white));
+                        placeNewPiece('f', 1, new Bishop(board, white));
+                        break;
+                    case 6:
+                        placeNewPiece('g', 8, new Knight(board, black));
+                        placeNewPiece('g', 7, new Pawn(board, black));
+                        placeNewPiece('g', 2, new Pawn(board, white));
+                        placeNewPiece('g', 1, new Knight(board, white));
+                        break;
+                    case 7:
+                        placeNewPiece('h', 8, new Rook(board, black));
+                        placeNewPiece('h', 7, new Pawn(board, black));
+                        placeNewPiece('h', 2, new Pawn(board, white));
+                        placeNewPiece('h', 1, new Rook(board, white));
+                        break;
             }
-            else if(i == 3){
-                setupPiece('k', i);
-            }
-            else if(i == 0 || i == 7){
-                setupPiece('r', i);
-            }
-            else if(i == 1 || i == 6){
-                setupPiece('n', i);
-            }
-            else if(i == 2 || i == 5){
-                setupPiece('b', i);
-            }
-            
-            setupPiece('p', i);
         }
         //BoardException test:
         //board.placePiece(new Rook(board, Color.BLACK), new Position(0, 8));
     }
 
     
-    private void setupPiece (char type, int column){
+    private void setupPiece (char type, char column){
         switch (type){
             case 'r':
                 board.placePiece(new Rook(board, Color.BLACK), new Position(0, column));
@@ -90,4 +130,33 @@ public class ChessMatch {
                 break;
         }
     }
+    
+//    private void setupPiece (char type, int column){
+//        switch (type){
+//            case 'r':
+//                board.placePiece(new Rook(board, Color.BLACK), new Position(0, column));
+//                board.placePiece(new Rook(board, Color.WHITE), new Position(7, column));
+//                break;
+//            case 'n':
+//                board.placePiece(new Knight(board, Color.BLACK), new Position(0, column));
+//                board.placePiece(new Knight(board, Color.WHITE), new Position(7, column));
+//                break;
+//            case 'b':
+//                board.placePiece(new Bishop(board, Color.BLACK), new Position(0, column));
+//                board.placePiece(new Bishop(board, Color.WHITE), new Position(7, column));
+//                break;
+//            case 'k':
+//                board.placePiece(new King(board, Color.BLACK), new Position(0, column));
+//                board.placePiece(new King(board, Color.WHITE), new Position(7, column));
+//                break;
+//            case 'q':
+//                board.placePiece(new Queen(board, Color.BLACK), new Position(0, column));
+//                board.placePiece(new Queen(board, Color.WHITE), new Position(7, column));
+//                break;
+//            case 'p':
+//                board.placePiece(new Pawn(board, Color.BLACK), new Position(1, column));
+//                board.placePiece(new Pawn(board, Color.WHITE), new Position(6, column));
+//                break;
+//        }
+//    }
 }
